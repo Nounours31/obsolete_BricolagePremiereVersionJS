105a4f59a78ba3aa1b138fd53763ea04271a0cea
Nounours31 <code.fages@gmail.com>
2019-06-25 17:38:09 +0200

    ODT linux

--- ODT/BuildODT.sh
+++ ODT/BuildODT.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 
-export PATH=${PATH};C:\java\jdk1.8.0_144\bin;C:\java\jdk1.8.0_144
-export JAVA_HOME=C:\java\jdk1.8.0_144
+export PATH=${PATH}:/usr/bin
+export JAVA_HOME=/usr/bin
  
-export MYCLASSPATH=build\*
+export MYCLASSPATH=build/*
 
 mkdir build
 rm -rf build/*
 
 for i in infra main test; do 
-	rm -rf build\$i
-	mkdir build\%%i
-	javac -d build\%%i -cp .;%MYCLASSPATH%;thdpty\JUnit-4.13\*;thdpty\selenium-java-3.141.59\libs\*;thdpty\selenium-java-3.141.59\* src\bri\brico2\selenium\%%i\*.java 
-	cd build\%%i
-	jar cvf %%i.jar *
+	rm -rf build/$i
+	mkdir build/$i
+	javac -d build/$1 -cp .;${MYCLASSPATH};thdpty/JUnit-4.13/*;thdpty/selenium-java-3.141.59/libs/*;thdpty/selenium-java-3.141.59/* src/bri/brico2/selenium/$i/*.java 
+	cd build/$i
+	jar cvf $i.jar *
 	cd ..
-	mv %%i\%%i.jar .
-	rm -rf %%i
+	mv $i/$i.jar .
+	rm -rf $i
 	cd ..
 done
+
+
+
+
 
