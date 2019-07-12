8edb59051f6dfae291d0f8b773b0fea061bdd75e
Nounours31 <code.fages@gmail.com>
2019-07-12 12:43:53 +0200

    ODT

--- ODT/BuildODT.sh
+++ ODT/BuildODT.sh
@@ -3,27 +3,24 @@
 export PATH=${PATH}:/usr/bin
 export JAVA_HOME=/usr/bin
 
-export MYCLASSPATH=build/*
+export BUILD_PLACE=./build_odt
+export MYCLASSPATH=${BUILD_PLACE}/*
 
-if [ -d ./build ]; then
-        rm -rf build
+if [ -d ${BUILD_PLACE} ]; then
+        rm -rf ${BUILD_PLACE}
 fi
-
-mkdir build
+mkdir ${BUILD_PLACE}
 
 set -x
 for i in infra main test; do
-        [ -d build/$i ] && rm -rf build/$i
-        mkdir build/$i
-        javac -d build/$i -cp .:${MYCLASSPATH}:thdpty/JUnit-4.13/*:thdpty/selenium-java-3.141.59/libs/*:thdpty/selenium-java-3.141.59/* src/bri/brico2/selenium/$i/*.java
-        cd build/$i
-        jar cvf $i.jar *
-        cd ..
-        mv $i/$i.jar .
-        rm -rf $i
-        cd ..
+  echo "ODT Build of $i"
+  [ -d ${BUILD_PLACE}/$i ] && rm -rf ${BUILD_PLACE}/$i
+  mkdir ${BUILD_PLACE}/$i
+  javac -d ${BUILD_PLACE}/$i -cp .:${MYCLASSPATH}:thdpty/JUnit-4.13/*:thdpty/selenium-java-3.141.59/libs/*:thdpty/selenium-java-3.141.59/* src/bri/brico2/selenium/$i/*.java
+  cd ${BUILD_PLACE}/$i
+  jar cvf $i.jar *
+  cd ..
+  mv $i/$i.jar .
+  rm -rf $i
+  cd ..
 done
-
-
-
-
