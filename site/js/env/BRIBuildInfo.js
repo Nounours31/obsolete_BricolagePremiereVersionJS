511c7f38dc2b443ffb3914e4c6393787e199f99b
Nounours31 <mobile.fages@gmail.com>
2019-08-29 16:00:05 +0200

    auto build 325 - COMMIT

--- site/js/env/BRIBuildInfo.js
+++ site/js/env/BRIBuildInfo.js
@@ -2,9 +2,9 @@
 
 class BRIBuildInfo {
   constructor() {
-    this._dateBuild = '2019-08-29 12:00:05';
-    this._versionBuild = 'jenkins-Bricolage-324';
-    this._commitGIT = 'd5a647d80a1d4d49ac0c0ac448a9472938252af8';
+    this._dateBuild = '2019-08-29 16:00:05';
+    this._versionBuild = 'jenkins-Bricolage-325';
+    this._commitGIT = '694856a3026e203440bc60e0af134df3f6cbac71';
   }
 
   getBuildInfo () {
