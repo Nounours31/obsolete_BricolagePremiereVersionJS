b9f1db322beb2f46ff24b041ee4b47b7de10581b
Nounours31 <mobile.fages@gmail.com>
2020-10-23 20:00:07 +0200

    auto build 2010 - COMMIT

--- site/js/env/BRIBuildInfo.js
+++ site/js/env/BRIBuildInfo.js
@@ -2,9 +2,9 @@
 
 class BRIBuildInfo {
   constructor() {
-    this._dateBuild = '2020-10-23 16:00:08';
-    this._versionBuild = 'jenkins-Bricolage-2009';
-    this._commitGIT = 'aa35454b0fd8059c1f5af90cafe50ac75d7fb4c8';
+    this._dateBuild = '2020-10-23 20:00:07';
+    this._versionBuild = 'jenkins-Bricolage-2010';
+    this._commitGIT = '132bf6f94468b174ddbada8d3a406684004374a2';
   }
 
   getBuildInfo () {
